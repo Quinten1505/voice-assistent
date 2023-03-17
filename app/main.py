28f1ed4b0055027384087d7f01ae01fc0e217b22
Quinten <Quinten1505@gmail.com>
2023-03-17 07:46:55 +0100

    Added request

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -11,9 +11,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # initialize the directory './templates' which holds the html files
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello FastAPI"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
